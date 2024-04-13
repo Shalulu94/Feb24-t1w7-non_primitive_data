1377657195293dabe49361e1aed29215eeecf1b9
shalulu94 <shahrulnasir290@hotmail.com>
2024-04-13 14:17:21 +1000

    challenges

---
+++ challenge1.py
@@ -0,0 +1,3 @@
+# https://leetcode.com/problems/maximum-number-of-words-found-in-sentences/
+# Please post your solution here, wrapped in triple backticks (`) and double vertical bars (|), for code and hiding it respectively.
+# These formatting options can also be accessed when you highlight your text.
---
+++ challenge2.py
@@ -0,0 +1,3 @@
+# https://leetcode.com/problems/concatenation-of-array/
+# Please post your solution here, wrapped in triple backticks (`) and double vertical bars (|), for code and hiding it respectively.
+# These formatting options can also be accessed when you highlight your text.
